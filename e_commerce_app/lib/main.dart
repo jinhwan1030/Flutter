91baf1de564185c1918015cbffc1893269c9586e
jinhwan1030 <81551846+airtech306@users.noreply.github.com>
2023-12-07 15:05:13 +0900

    Commerce-app init
    Commerce-app initialize

---
+++ e_commerce_app/lib/main.dart
@@ -0,0 +1,17 @@
+import 'presentation/routes/routes.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      routerConfig: router,
+    );
+  }
+}
---
+++ e_commerce_app/lib/presentation/pages/splash/splash_page.dart
@@ -0,0 +1,38 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:go_router/go_router.dart';
+
+import '../../routes/route_path.dart';
+
+//TODO 로그인 bloc
+class SplashPage extends StatefulWidget {
+  const SplashPage({super.key});
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Timer(
+      Duration(seconds: 2),
+      () => context.go(RoutePath.home),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      //TODO 하드코딩 된 값 변경할 것
+      body: Center(
+        child: SvgPicture.asset('assets/svg/main_logo.svg'),
+      ),
+      backgroundColor: Color(0xFF5F0080),
+    );
+  }
+}
